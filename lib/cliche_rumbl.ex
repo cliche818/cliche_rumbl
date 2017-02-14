e1f71ec6acaf4fc3223af696727f43bb3a4cd7c4
Cliche818 <jeff.chun.yinli@gmail.com>
2017-02-13 23:05:06 -0500

    add ecto.repo + add user migration

--- lib/cliche_rumbl.ex
+++ lib/cliche_rumbl.ex
@@ -9,7 +9,7 @@ defmodule ClicheRumbl do
     # Define workers and child supervisors to be supervised
     children = [
       # Start the Ecto repository
-#      supervisor(ClicheRumbl.Repo, []),
+      supervisor(ClicheRumbl.Repo, []),
       # Start the endpoint when the application starts
       supervisor(ClicheRumbl.Endpoint, []),
       # Start your own worker by calling: ClicheRumbl.Worker.start_link(arg1, arg2, arg3)
--- web/models/user.ex
+++ web/models/user.ex
@@ -1,3 +1,12 @@
 defmodule ClicheRumbl.User do
-  defstruct [:id, :name, :username, :password]
+  use ClicheRumbl.Web, :model
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps
+  end
 end
