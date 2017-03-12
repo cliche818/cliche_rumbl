518625f34316faf592690f92880eb2887bcffbe3
Cliche818 <jeff.chun.yinli@gmail.com>
2017-03-11 23:41:53 -0500

    created category model for videos + seed some initial categories

--- web/models/video.ex
+++ web/models/video.ex
@@ -6,6 +6,7 @@ defmodule ClicheRumbl.Video do
     field :title, :string
     field :description, :string
     belongs_to :user, ClicheRumbl.User
+    belongs_to :category, ClicheRumbl.Category
 
     timestamps()
   end
@@ -15,7 +16,7 @@ defmodule ClicheRumbl.Video do
   """
 
   @required_fields ~w(url title description)
-  @optional_fields  ~w()
+  @optional_fields  ~w(category_id)
 
   def changeset(struct, params \\ %{}) do
     struct
