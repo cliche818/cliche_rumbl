625d18d4f7fae8babaed56fe1cac83af37b63dcc
Cliche818 <jeff.chun.yinli@gmail.com>
2017-04-30 00:43:23 -0400

    added repo tests for user and category to test with side effects

---
+++ test/models/category_repo_test.exs
@@ -0,0 +1,15 @@
+defmodule ClicheRumbl.CategoryRepoTest do
+  use ClicheRumbl.ModelCase
+
+  alias ClicheRumbl.Category
+
+  test "alphabetical/1 orders by name" do
+    Repo.insert!(%Category{name: "c"})
+    Repo.insert!(%Category{name: "a"})
+    Repo.insert!(%Category{name: "b"})
+
+    query = Category |> Category.alphabetical()
+    query = from c in query, select: c.name
+    assert Repo.all(query) == ~w(a b c)
+  end
+end
