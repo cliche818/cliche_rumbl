60caf048b1e17d3da195de65006e522d9ecaf1bb
Cliche818 <jeff.chun.yinli@gmail.com>
2017-06-11 21:27:53 -0400

    created alias for video links (making a custom id)

--- web/models/video.ex
+++ web/models/video.ex
@@ -1,10 +1,13 @@
 defmodule ClicheRumbl.Video do
   use ClicheRumbl.Web, :model
 
+  @primary_key {:id, ClicheRumbl.Permalink, autogenerate: true}
+
   schema "videos" do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
     belongs_to :user, ClicheRumbl.User
     belongs_to :category, ClicheRumbl.Category
 
@@ -15,10 +18,33 @@ defmodule ClicheRumbl.Video do
   Builds a changeset based on the `struct` and `params`.
   """
 
-  def changeset(struct, params \\ %{}) do
-    struct
-    |> cast(params, [:url, :title, :description, :category_id])
-    |> validate_required([:url, :title, :description])
+  @required_fields ~w(url title description)
+  @optional_fields  ~w(category_id)
+
+  def changeset(model, params \\ %{}) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+    |> slugify_title()
     |> assoc_constraint(:category)
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
+  end
+
+  defimpl Phoenix.Param, for: ClicheRumbl.Video do
+    def to_param(%{slug: slug, id: id}) do
+      "#{id}-#{slug}"
+    end
   end
 end
